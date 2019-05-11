257f786c66abef080ac1023d5a3a653c5cf50644
Konstantin Kirik (snegovick) <snegovick@uprojects.org>
2019-05-11 23:11:23 +0300

    Rewrote to distutils setup code

--- pytorch_binding/build.py
+++ pytorch_binding/build.py
@@ -1,7 +1,44 @@
 import os
 import sys
 import torch
-from torch.utils.ffi import create_extension
+# from torch.utils.ffi import create_extension
+
+# this_file = os.path.abspath(__file__)
+# warp_root = os.path.dirname(os.path.dirname(this_file))
+
+# sources = ['src/warp_ctc.c']
+# headers = ['src/warp_ctc.h']
+# include_dirs = [os.path.join(warp_root, 'include')]
+# libraries = ['warpctc']
+# library_dirs = [os.path.join(warp_root, 'build')]
+# defines = []
+# with_cuda = False
+
+# if torch.cuda.is_available():
+#     print('Including CUDA code.')
+#     sources += ['src/warp_ctc_cuda.c']
+#     headers += ['src/warp_ctc_cuda.h']
+#     defines += [('WITH_CUDA', None)]
+#     with_cuda = True
+
+#     if len(sys.argv) > 1:
+#         include_dirs += [sys.argv[1]]
+
+# ffi = create_extension(
+#     '_ext.ctc',
+#     headers=headers,
+#     sources=sources,
+#     include_dirs=include_dirs,
+#     relative_to=__file__,
+#     define_macros=defines,
+#     with_cuda=with_cuda,
+#     libraries=libraries,
+#     library_dirs=library_dirs
+# )
+
+# ffi.build()
+
+from distutils.core import setup, Extension
 
 this_file = os.path.abspath(__file__)
 warp_root = os.path.dirname(os.path.dirname(this_file))
@@ -24,16 +61,14 @@ if torch.cuda.is_available():
     if len(sys.argv) > 1:
         include_dirs += [sys.argv[1]]
 
-ffi = create_extension(
-    '_ext.ctc',
-    headers=headers,
-    sources=sources,
-    include_dirs=include_dirs,
-    relative_to=__file__,
-    define_macros=defines,
-    with_cuda=with_cuda,
-    libraries=libraries,
-    library_dirs=library_dirs
+setup(
+    name='_ext.ctc',
+    version='1.0',
+    ext_modules=[Extension('_ext.ctc', sources,
+                           include_dirs=include_dirs,
+                           define_macros=defines,
+                           libraries=libraries,
+                           library_dirs=library_dirs,
+                           relative_to=__file__,
+                           with_cuda=with_cuda)],
 )
-
-ffi.build()
